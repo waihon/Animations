8bd9fc3c70a25c0647e0a8d855ad9ebe72a909b9
Waihon Yew <yewwaihon@gmail.com>
2021-06-02 22:57:13 +0800

    Make the animation overshoot then return to settle at its target

--- Animations/ContentView.swift
+++ Animations/ContentView.swift
@@ -19,7 +19,7 @@ struct ContentView: View {
     .foregroundColor(.white)
     .clipShape(Circle())
     .scaleEffect(animationAmount)
-    .animation(.easeOut)
+    .animation(.interpolatingSpring(stiffness: 50, damping: 1))
   }
 }
 
